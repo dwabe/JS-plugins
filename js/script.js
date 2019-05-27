eed99283c376359a56d432fa53bc15d94de4c4db
Bartek Błęcki <bartek@blecki.pl>
2019-05-27 12:10:50 +0200

    Add progress bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-var elem = document.querySelector('.main-carousel');
+var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
   hash: true
+});
+
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
